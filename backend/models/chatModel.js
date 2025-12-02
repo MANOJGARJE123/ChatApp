82cd8bc2267b736406c178e9cfa932df05cb1bc4
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-02 10:55:26 +0530

    Adds: Chat model

---
+++ backend/models/chatModel.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const chatSchema = new mongoose.Schema({
+    users : [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ],
+    latestMessage :{
+        String : text,
+
+        sender:{
+            type: mongoose.Schema.Tpyes.ObjectId,
+            ref:"User"
+        }
+    }
+},{
+    timestamps: true
+})
+
+const Chat = mongoose.model('Chat', chatSchema);
+
+module.exports = chat;
