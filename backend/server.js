3539bff53cc7eda9d20c6cf2c57cce37dd0135c5
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-02 10:14:22 +0530

    Backend: Initial setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-improt 
+import cors from 'cors'
+import express from  'express'
+
+const port = process.env.PORT || 5000;
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`server listening on port${port}`)
+})
