937c75a98ed88e02c1ee9875ba560b40f0230ba7
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-02 10:37:14 +0530

    Create: Server

---
+++ backend/database/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectdb = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            dbName : "ChatApp"
+        })
+        console.log("connect to MongoDB")
+    }catch{
+        console.log(error)
+    }   
+}
