500d43f586cfa6d5cf1dc92e48edfe302e1a64aa
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-04 09:45:20 +0530

    adds(feat) : Send message

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,25 +1,25 @@
 import Message from '../models/messageModle'
 import Chat from '../models/chatModel'
 
-export const message = async(req, res) =>{
+export const sendMessage = async(req, res) =>{
     try{
         const {chatId, sender, text} = req.body
 
         if(!chatId || !sender || !text){
-            res.status(400).json({message : "All feilds are required"})
+            return res.status(400).json({message : "All feilds are required"})
         }
 
-        const message = await message.create({
+        const newMessage = await Message.create({
             chatId,
             sender,
-            message
+            text
         })
 
         return res.status(200).json({
             messsage : "Message sent",
-            data : message
+            data : newMessage
         })
     }catch (error){
-        res.status(500).json({message:"Something went wrong", error})
+        return res.status(500).json({message:"Something went wrong", error})
     }
-}
+}   
