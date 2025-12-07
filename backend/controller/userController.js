a69052a05b0107a6258143bb24a4513002ea7520
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-07 12:03:03 +0530

    Adds(feat) :userController/searchUser

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -26,7 +26,7 @@ export const getUserById = async(req, res) =>{
     try{
         const {id} = req.params
 
-        const user = await user.findById(user).select('-password')
+        const user = await User.findById(user).select('-password')
 
         if(!user){
             return res.status(404).json({message: "User not found"})
@@ -35,5 +35,18 @@ export const getUserById = async(req, res) =>{
         res.status(200).json(user);
     }catch(error){
         return res.status(500).json({message : "something went wrong"})
+    }
+}
+
+export const searchUser = async(req, res) =>{
+    try{
+        const {query} = req.query
+        const users = await User.find({
+            name :{$regex: query, $options: "i"}
+        }).select("-password")
+
+        res.status(200).json(users)
+    }catch(error){
+        res.status(500).json({ message: error.message });
     }
 }
