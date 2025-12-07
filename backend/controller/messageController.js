31871d30dcd0267ce0cc871ecc2a89c03ad185d4
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-07 14:52:29 +0530

    Changes: Message controller

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,28 +1,65 @@
 import Message from '../models/messageModle'
 import Chat from '../models/chatModel'
 
-export const sendMessage = async(req, res) =>{
-    try{
-        const {chatId, sender, text} = req.body
+export const sendMessage = async (req, res) => {
+    try {
+        const { chatId, sender, text } = req.body;
 
-        if(!chatId || !sender || !text){
-            return res.status(400).json({message : "All feilds are required"})
+        if (!chatId || !sender || !text) {
+            return res.status(400).json({ message: "All fields are required" });
         }
 
         const newMessage = await Message.create({
             chatId,
             sender,
             text
-        })
+        });
+
+        await newMessage.populate('sender', 'name email');
+
+        await Chat.findByIdAndUpdate(chatId, {
+            latestMessage: {
+                text: text,
+                sender: sender
+            }
+        });
+
+        const chat = await Chat.findById(chatId);
+        if (chat) {
+            const receiverId = chat.users.find(userId => userId.toString() !== sender.toString());
+            
+            if (receiverId) {
+                const receiverSocketId = getReceiverSocketId(receiverId);
+                const io = getIO();
+
+                if (receiverSocketId && io) {
+                    io.to(receiverSocketId).emit('newMessage', {
+                        id: newMessage._id,
+                        chatId: newMessage.chatId,
+                        sender: {
+                            _id: newMessage.sender._id,
+                            name: newMessage.sender.name,
+                            email: newMessage.sender.email
+                        },
+                        text: newMessage.text,
+                        timestamp: newMessage.createdAt
+                    });
+                }
+            }
+        }
 
         return res.status(200).json({
-            messsage : "Message sent",
-            data : newMessage
-        })
-    }catch (error){
-        return res.status(500).json({message:"Something went wrong", error})
+            message: "Message sent",
+            data: newMessage
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            message: "Something went wrong",
+            error
+        });
     }
-}   
+};
 
 export const getMessagebyChatId = async(req, res) =>{
     try{
