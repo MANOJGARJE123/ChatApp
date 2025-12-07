c3cfe6f15a52a81ed2c3014ef5c465499b591870
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-07 11:14:36 +0530

    Adds(feat): getUserById

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -22,3 +22,18 @@ export const getAllUsers = async(req, res) =>{
         res.status(500).json({ message: "No user are there" });
     }
 }
+export const getUserByid = async(req, res) =>{
+    try{
+        const {id} = req.params
+
+        const user = await user.findById(user).select('-password')
+
+        if(!user){
+            return res.status(404).json({message: "User not found"})
+        }
+
+        res.status(200).json(user);
+    }catch(error){
+        return res.status(500).json({message : "something went wrong"})
+    }
+}
