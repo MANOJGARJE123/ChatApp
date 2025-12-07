961779a4c7cd02cfa183d264ad4e1f2861a018bf
MANOJGARJE123 <manojgarje123@gmail.com>
2025-12-07 12:11:19 +0530

    Add(feat) : userContoller/deletUser

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -67,3 +67,13 @@ export const updateUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.user.id);
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
